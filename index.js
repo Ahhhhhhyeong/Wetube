de1617c2ac52886d9db47b0908a69769f0bc92bf
Ahhhhhhyeong <dkdud081@gmail.com>
2020-07-07 19:40:02 +0900

    Add nodemon

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handelListing(){
+const handelListing = () =>{
     console.log(`Listening on: http://localhost:${PORT} `)
 }
+
+const handelHome = (req, res) => res.send('HI FROM HOME!');
+
+const handelProfile = (req, res) => res.send("YOU ARE ON MY PROFILE"); //arrow function
+
+app.get("/", handelHome);
+
+app.get("/profile", handelProfile);
 
 app.listen(4000, handelListing);
