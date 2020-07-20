1695acbb120c32755e6208d6a99194c4acf35675
Ahhhhhhyeong <dkdud081@gmail.com>
2020-07-20 15:35:42 +0900

    corrected 404 error

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,12 +4,9 @@ import routes from "../routes";
 const userRouter = express.Router();
 
 userRouter.get(routes.users, (req, res) => res.send("USERS"));
-
+userRouter.get(routes.userDetail, (req, res) => res.send("UserDetail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("EditProfile"));
+userRouter.get(routes.changePassword, (req, res) => res.send("ChagePassword"));
 
 export default userRouter;
 
-/*
-Model data
-View how does the data look
-funCtion that looks for the data
-*/
