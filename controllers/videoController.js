4e38646664037ef56f345131ab0f9597b2d8bd97
Ahhhhhhyeong <dkdud081@gmail.com>
2020-07-28 15:10:04 +0900

    make View with pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,13 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videosDetail = (req, res) => res.render("videosDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "HOME" });
+export const search = (req, res) => {
+   //const searchingBy = req.query.term;
+    const { 
+        query: { term: searchingBy } 
+    } = req;
+    res.render("search", { pageTitle: "SEARCH", searchingBy });
+};
+export const videos = (req, res) => res.render("videos", { pageTitle: "VIDEOS" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "UPLOADE" });
+export const videosDetail = (req, res) => res.render("videosDetail", { pageTitle: "VIDEOS DETAIL" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "EDIT VIDEO" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "DELETE VIDEO" });
