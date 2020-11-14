12bf377fc96c03bbd650e73903cf606413d99f42
Ahhhhhhyeong <dkdud081@gmail.com>
2020-11-14 16:17:06 +0900

    Github LogIn, Logout

--- routes.js
+++ routes.js
@@ -18,6 +18,10 @@ const VIDEOS_DETAIL ="/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
+// Github
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
 
 const routes = {
     home: HOME,
@@ -58,7 +62,9 @@ const routes = {
         else{
             return DELETE_VIDEO;
         }
-    }
+    },
+    gitHub: GITHUB,
+    githubCallback: GITHUB_CALLBACK
 };
 
 export default routes;
