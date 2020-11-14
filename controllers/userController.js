e125371ded70b81155aa8076d27f6b65a44c86fa
Ahhhhhhyeong <dkdud081@gmail.com>
2020-11-14 13:47:18 +0900

    MongoStore and Middlewares

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,12 @@
+import passport from "passport";
 import routes from "../routes";
+import User from "../models/User";
 
-export const postJoin= (req, res) => {
+export const getJoin = (req, res) => {
+    res.render("join", { pageTitle: "JOIN" });
+};
+
+export const postJoin= async (req, res, next) => {
     const {
         body: { name, email, password, password2 }
     } = req;
@@ -8,24 +14,27 @@ export const postJoin= (req, res) => {
        // res.status(400);
         res.render("join", { pageTitle: "Join" });
     } else {
-        // To Do: Register User
-        // To Do: Log user in
-        res.redirect(routes.home);
+        try {
+            const user = await User({
+              name,
+              email
+            });
+            await User.register(user, password);
+            next();
+          } catch (error) {
+            console.log(error);
+            res.redirect(routes.home);
+          }
     }
 };
-
-export const getJoin = (req, res) => {
-    res.render("join", { pageTitle: "JOIN" });
-};
-
 
 export const getLogin = (req, res) => {
     res.render("login", {pageTitle: "LogIn"});
 };
-export const postLogin = (req, res) => {
-    //  res.render("login", {pageTitle: "LogIn"});
-    res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate("local", {
+    failureRedirect: routes.login,
+    successRedirect: routes.home
+  });
 
 export const logout = (req, res) => {
     // To Do: Process Log Out
